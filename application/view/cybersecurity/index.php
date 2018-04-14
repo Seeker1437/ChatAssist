3931f00dc0abeaae6d47aecd6e52b24051c5346a
Jeff Ginsburg <ginsburgj@surgent.com>
2018-04-14 13:54:44 -0400

    Jeff - Cybersecurity page

---
+++ application/view/cybersecurity/index.php
@@ -0,0 +1,7 @@
+<div class="container chat">
+    <div class="row">
+          <div class="col-md-12 logo"></div>
+          <a href="<?php echo Config::get('URL'); ?>application/view/landing">Back</a>
+          <div class="col-md-12">CYBERSECURITY CHAT HERE</div>
+    </div>
+</div>
--- application/view/landing/index.php
+++ application/view/landing/index.php
@@ -2,7 +2,7 @@
     <div class="row">
           <div class="col-md-12 logo"></div>
           <div class="landing-page-row col-md-12"><i class="fa fa-database"></i> Data Privacy <i class="fa fa-angle-right"></i></div>
-          <div class="landing-page-row col-md-12"><i class="fa fa-lock"></i> Cybersecurity <i class="fa fa-angle-right"></i></div>
+          <a href="<?php echo Config::get('URL'); ?>application/view/cybersecurity"><div class="landing-page-row col-md-12"><i class="fa fa-lock"></i> Cybersecurity <i class="fa fa-angle-right"></i></div></a>
           <div class="landing-page-row col-md-12"><i class="fa fa-user"></i> Developer Onboarding <i class="fa fa-angle-right"></i></div>
           <div class="landing-page-row col-md-12"><i class="fa fa-shopping-cart"></i> Vendor Onboarding <i class="fa fa-angle-right"></i></div>
     </div>
