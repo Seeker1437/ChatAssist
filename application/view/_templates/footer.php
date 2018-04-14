8f3b98c462cd7a4af8949e0c404cc46f18c89b0b
Darnell Williams <williams.darnell56@yahoo.com>
2018-04-14 14:39:18 -0400

    Updates bot to conform to framework

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -4,5 +4,31 @@
         <script src="<?php echo Config::get('URL'); ?>public/js/bootstrap.min.js"></script>
         <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
         <script src="<?php echo Config::get('URL'); ?>public/js/ie10-viewport-bug-workaround.js"></script>
+        <?php if ($this->checkForActiveControllerAndAction("cybersecurity/index")) { ?>
+        <script src="https://sdk.amazonaws.com/js/aws-sdk-2.41.0.min.js"></script>
+            <script type="text/javascript">
+                $(document).ready(function() {
+                    $("#signup").click(function() {
+                        $.post( "/signup", $("#signupForm").serialize(),
+                            function(data) {
+                                $("#signupSuccess").show();
+                            }
+                        )
+                            .error(function(xhr) {
+                                switch(xhr.status) {
+                                    case 409:
+                                        $("#signupDuplicate").show();
+                                        break;
+                                    default:
+                                        $("#signupError").show();
+                                }
+                            })
+                            .always(function() {
+                                $("#signupModal").modal('hide');
+                            });
+                    })
+                })
+            </script>
+        <?php } ?>
     </body>
 </html>
