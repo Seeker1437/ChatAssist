ca6542ccce104281c634e96d5252266bdb9b89a9
Jeff Ginsburg <ginsburgj@surgent.com>
2018-04-14 12:52:30 -0400

    Jeff - Landing page update

--- application/view/landing/index.php
+++ application/view/landing/index.php
@@ -1,20 +1,10 @@
-<style>
-    body {
-        background: #4f2173 url('<?= Config::get('URL').'public/img/home_bg.jpg'?>');
-    }
-</style>
 <div class="container">
-    <h1 class="text-center" style="color: white">Landing Page</h1>
+    <h1 class="text-center">Landing Page</h1>
     <div class="row">
-        <div class="col-xs-6">
-            <a href="http://www.alfc-pa.org">
-                <img src="<?php echo Config::get('URL') ?>public/img/alfc_pa.png" class="img-responsive img-circle" />
-            </a>
-        </div>
-        <div class="col-xs-6">
-            <a href="http://www.alfc-pa.org">
-                <img src="<?php echo Config::get('URL') ?>public/img/alfc_nj.png" class="img-responsive img-circle" />
-            </a>
-        </div>
+          <div class="col-md-12">Logo</div>
+          <div class="landing-page-row col-md-12"><i class="fa-database"></i> Data Privacy <i class="fa-angle-right"></i></div>
+          <div class="landing-page-row col-md-12"><i class="fa-lock"></i> Cybersecurity <i class="fa-angle-right"></i></div>
+          <div class="landing-page-row col-md-12"><i class="fa-user"></i> Developer Onboarding <i class="fa-angle-right"></i></div>
+          <div class="landing-page-row col-md-12"><i class="fa-shopping-cart"></i> Vendor Onboarding <i class="fa-angle-right"></i></div>
     </div>
 </div>
