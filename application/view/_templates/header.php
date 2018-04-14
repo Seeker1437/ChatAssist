85e54920bf3cf2103fd2f27590de23e52b2454a1
Jeff Ginsburg <ginsburgj@surgent.com>
2018-04-14 13:07:37 -0400

    Jeff - landing page update

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ChatAssist</title>
+    <link href="<?php echo Config::get('URL'); ?>public/css/styles.css" rel="stylesheet">
     <link href="<?php echo Config::get('URL'); ?>public/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo Config::get('URL'); ?>public/fonts/font-awesome.min.css" rel="stylesheet">
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
--- application/view/landing/index.php
+++ application/view/landing/index.php
@@ -1,7 +1,7 @@
-<div class="container">
+<div class="container landing">
     <h1 class="text-center">Landing Page</h1>
     <div class="row">
-          <div class="col-md-12">Logo</div>
+          <div class="col-md-12 logo"></div>
           <div class="landing-page-row col-md-12"><i class="fa-database"></i> Data Privacy <i class="fa-angle-right"></i></div>
           <div class="landing-page-row col-md-12"><i class="fa-lock"></i> Cybersecurity <i class="fa-angle-right"></i></div>
           <div class="landing-page-row col-md-12"><i class="fa-user"></i> Developer Onboarding <i class="fa-angle-right"></i></div>
