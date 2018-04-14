fafba172a7ab9e31d0275c50337d5401a800da3b
Darnell Williams <williams.darnell56@yahoo.com>
2018-04-14 14:00:23 -0400

    Removes controller protection

--- application/controller/LandingController.php
+++ application/controller/LandingController.php
@@ -11,9 +11,6 @@ class LandingController extends Controller
     public function __construct()
     {
         parent::__construct();
-
-        // this entire controller should only be visible/usable by logged in users, so we put authentication-check here
-        Auth::checkAuthentication();
     }
 
     /**
