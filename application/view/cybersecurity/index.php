750ca08330eaa27c1bad9cfb8ab211fa6e3db550
Darnell Williams <williams.darnell56@yahoo.com>
2018-04-14 13:58:17 -0400

    update links

--- application/view/cybersecurity/index.php
+++ application/view/cybersecurity/index.php
@@ -1,7 +1,7 @@
 <div class="container chat">
     <div class="row">
           <div class="col-md-12 logo"></div>
-          <a href="<?php echo Config::get('URL'); ?>application/view/landing">Back</a>
+          <a href="<?php echo Config::get('URL'); ?>">Back</a>
           <div class="col-md-12">CYBERSECURITY CHAT HERE</div>
     </div>
 </div>
