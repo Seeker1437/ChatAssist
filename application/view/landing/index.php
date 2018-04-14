60519842a03d151105ea3c90f7a12cb5f6f1a5dd
Jeff Ginsburg <ginsburgj@surgent.com>
2018-04-14 13:40:05 -0400

    Jeff - Landing CSS Updates

--- application/view/landing/index.php
+++ application/view/landing/index.php
@@ -1,10 +1,9 @@
 <div class="container landing">
-    <h1 class="text-center">Landing Page</h1>
     <div class="row">
           <div class="col-md-12 logo"></div>
-          <div class="landing-page-row col-md-12"><i class="fa-database"></i> Data Privacy <i class="fa-angle-right"></i></div>
-          <div class="landing-page-row col-md-12"><i class="fa-lock"></i> Cybersecurity <i class="fa-angle-right"></i></div>
-          <div class="landing-page-row col-md-12"><i class="fa-user"></i> Developer Onboarding <i class="fa-angle-right"></i></div>
-          <div class="landing-page-row col-md-12"><i class="fa-shopping-cart"></i> Vendor Onboarding <i class="fa-angle-right"></i></div>
+          <div class="landing-page-row col-md-12"><i class="fa fa-database"></i> Data Privacy <i class="fa fa-angle-right"></i></div>
+          <div class="landing-page-row col-md-12"><i class="fa fa-lock"></i> Cybersecurity <i class="fa fa-angle-right"></i></div>
+          <div class="landing-page-row col-md-12"><i class="fa fa-user"></i> Developer Onboarding <i class="fa fa-angle-right"></i></div>
+          <div class="landing-page-row col-md-12"><i class="fa fa-shopping-cart"></i> Vendor Onboarding <i class="fa fa-angle-right"></i></div>
     </div>
 </div>
