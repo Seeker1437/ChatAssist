c78a222d9802e5f3587772bee0f7e391edbbf130
Jeff Ginsburg <ginsburgj@surgent.com>
2018-04-14 14:09:40 -0400

    Jeff - cs back button

--- application/view/cybersecurity/index.php
+++ application/view/cybersecurity/index.php
@@ -1,7 +1,7 @@
 <div class="container chat">
     <div class="row">
           <div class="col-md-12 logo"></div>
-          <a href="<?php echo Config::get('URL'); ?>">Back</a>
+          <a class="back-btn" href="<?php echo Config::get('URL'); ?>"><i class="fa fa-angle-left"></i>Back</a>
           <div class="col-md-12">CYBERSECURITY CHAT HERE</div>
     </div>
 </div>
