caa4205d43e577ce5627a2b38c8c5c2589eba618
Jeff Ginsburg <ginsburgj@surgent.com>
2018-04-15 09:59:12 -0400

    Jeff - coming soon update

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -2,5 +2,6 @@
     <div class="row">
         <div class="col-md-12 logo"></div>
         <a href="<?php echo Config::get('URL'); ?>cybersecurity"><div class="landing-page-row col-md-12"><i class="fa fa-lock"></i> Cybersecurity <i class="fa fa-angle-right"></i></div></a>
+        <div class="landing-page-row col-md-12"><i class="fa fa-ellipsis-h"></i> More Coming Soon <i class="fa fa-angle-right"></i></div>
     </div>
 </div>
