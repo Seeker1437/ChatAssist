1da1589065cc846c01fff2221217ff792a6675d6
Jeff Ginsburg <ginsburgj@surgent.com>
2018-04-15 08:49:18 -0400

    Jeff - home page update

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -5,5 +5,6 @@
         <a href="<?php echo Config::get('URL'); ?>cybersecurity"><div class="landing-page-row col-md-12"><i class="fa fa-lock"></i> Cybersecurity <i class="fa fa-angle-right"></i></div></a>
         <div class="landing-page-row col-md-12"><i class="fa fa-user"></i> Developer Onboarding <i class="fa fa-angle-right"></i></div>
         <div class="landing-page-row col-md-12"><i class="fa fa-shopping-cart"></i> Vendor Onboarding <i class="fa fa-angle-right"></i></div>
+        <div class="landing-page-row col-md-12"><i class="fa fa-globe"></i> Oil Spill Reporting <i class="fa fa-angle-right"></i></div>
     </div>
 </div>
